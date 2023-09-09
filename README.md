dfa801d1d793738ad1cf0e1eb38c8dd86374d9b1
Ashutosh Mishra <gsasha220@gmail.com>
2023-09-09 21:26:23 +0530

    nftmarketplaceadded

---
+++ README.md
@@ -0,0 +1,13 @@
+## Technology
+
+Tech uses:
+
+- Metamask
+- Truffle
+- Infuria
+- ReactJs
+- Tailwind CSS
+- Ganache-CLI
+- Solidity
+- Web3js
+- Faucet
---
+++ truffle-config.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+  // Configure networks (Localhost, Rinkeby, etc.)
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*', // Match any network id
+    },
+    
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: '0.8.11',
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+}
